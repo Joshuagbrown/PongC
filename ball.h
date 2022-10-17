d61a8bd4d0ed1fe117303e817b731e0a9cbe1db8
Maxzi Francisco <mfr91@uclive.ac.nz>
2022-10-17 19:00:10 +1300

    updated header

--- ball.h
+++ ball.h
@@ -1,17 +1,16 @@
 /**
  * @file ball.h
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2022-10-15
- * 
- * @copyright Copyright (c) 2022
- * 
+ * @author Joshua Brown, Maxzi Francisco
+ * @brief ball component of the game
+ * @date 17 October 2022
  */
+
 
 #ifndef BALL_H
 #define BALL_H
 
+
+/** Directions of the ball as coordinates */
 typedef struct {
     int8_t x;
     int8_t y;
@@ -19,6 +18,8 @@ typedef struct {
     int8_t vy;
 } Ball_t;
 
+
+/** States of the game*/
 typedef enum {
     STARTUP,
     SENDING,
@@ -27,34 +28,42 @@ typedef enum {
     GAMEOVER
 } state;
 
+
 /** Initialises ball coordinates using tinygl
-    @param tick
-    @param ball
-    @return 
+    @param tick timer
+    @param ball initial position of the ball
+    @return ball (with given coordinates)
 */
 Ball_t move_ball(int16_t* tick, Ball_t ball);
 
 
-
+/** Makes a new ball that's set to the default position
+    @return ball (with given coordinates)
+*/
 Ball_t reset_ball(void);
-/** Returns 
+
+
+/** Checks the position of the ball, if it hits a wall or if it needs to be transmitted to the other player's microcontoller 
     @param ball the position of the ball
     @return PLAYING (1) or SENDING (0)
 */
 int check_wall(Ball_t* ball);
-/** 
-    @param ball
-    @param retry
-    @return
+
+
+/** Sends ball to the other player and sets a retry
+    @param ball ball coordinates
+    @param retry to transmit data one more time
+    @return receiving state
 */
 int pass_ball(Ball_t ball, int* retry);
 
-/** 
-    @param state
-    @param retry
-    @return
+
+/** Receives ball transmission while the game is ongoing and makes a new ball if the game is over
+    @param state current state of the game
+    @param retry to transmit data one more time
+    @return returns a new ball for the retry
 */
 Ball_t wait_for_ball(int* state, int* retry);
 
 
-#endif
+#endif //BALL_H
--- text.h
+++ text.h
@@ -1,10 +1,33 @@
+/**
+ * @file text.h
+ * @author Joshua Brown, Maxzi Francisco
+ * @brief text to display component for the game
+ * @date 17 October 2022
+ 
+ */
+
+
 #ifndef TEXT_H
 #define TEXT_H
 
-#include <stdint.h>
 
+/** Displays characters for player options, used by char_in_bound function
+    @param character player type ('1' and '2') 
+ */
 void display_character (char character);
+
+
+/** Gets the player to choose as player 1 or 2 at the start of the game, player 2 as receiver and player 1 with the ball 
+    @param lowerBound 
+    @param upperBound 
+    @return chosen player type
+ */
+
 char char_in_bound(int8_t lowerBound, int8_t upperBound);
+
+
+/** Displays text updates about the game 
+ */
 void display_text (void);
 
 
