91bc139086558c32099c434f393afde757a4cbf8
Joshua Brown <jbr242@uclive.ac.nz>
2022-10-18 15:35:25 +1300

    paddle commenting

--- paddle.c
+++ paddle.c
@@ -20,21 +20,29 @@
 */
 void move_paddle(tinygl_point_t* left, tinygl_point_t* right)
 {
-    navswitch_update ();       
+    //Update the navswitch with the new state of line
+    navswitch_update ();
+    //Navswitch pushed South, move line south  
     if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
+        //Turn the current line off
         tinygl_draw_line(*left, *right, 0);
         if((*right).y < 6) {
+            //Set line to new position
             (*left).y ++;
             (*right).y ++;
         }       
     }     
+    //Navswitch pushed North, move line north 
     if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
+        //Turn the current line off
         tinygl_draw_line(*left, *right, 0);
         if((*left).y > 0) {
+            //Set line to new position
             (*left).y --;
             (*right).y --;
         }     
     }
+    //Turn line on at new position
     tinygl_draw_line(*left, *right, 1);
 }
 
@@ -47,20 +55,24 @@ void move_paddle(tinygl_point_t* left, tinygl_point_t* right)
 */
 int check_paddle(Ball_t* ball, tinygl_point_t left, tinygl_point_t right)
 {
+    //If ball hits the left side of the paddle, set the vy to left and vx flipped
     if((*ball).y == left.y) {
         (*ball).vy = -1;
         (*ball).vx = -1;
         return(PLAYING);
 
+    //If ball hits the middle of the paddle, set the velocity to left
     } if ((*ball).y == left.y + 1) {
         (*ball).vy = 0;
         (*ball).vx = -1;
         return(PLAYING);
 
+    //If ball hits the right, set vy to right, and vx flipped
     } if ((*ball).y == right.y) {
         (*ball).vy = 1;
         (*ball).vx = -1;
         return(PLAYING);
     }
+    //If the ball is not hitting the paddle, return game over.
     return(GAMEOVER);
 }
